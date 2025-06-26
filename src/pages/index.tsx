37082cfab289a68cbe59d228511a88a2e86b6019
Yan Carlos <yan312005@gmail.com>
2025-06-26 17:24:28 -0300

    fix: changed page rendering mode to ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "./home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
 interface HomeProps {
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1ReHP9P2LEVJwpZ063c8lNF3");
 
   const product = {
@@ -53,5 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
